b076233e77a63e78bae2c6c82e2200c2918157c8
aboutbus <aboutbus@localhost>
2016-07-19 17:53:21 +0300

    first commit

---
+++ httpsrv.c
@@ -0,0 +1,101 @@
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+char *host = "127.0.0.1";
+char *directory = "./";
+int port = 1080;
+
+void http_client(int nd)
+{
+	char buf[1024];
+
+	int size = recv(nd, buf, sizeof(buf), MSG_NOSIGNAL);
+	assert(size >= 0);
+	buf[size] = '\0';
+
+	printf("recv: %d\n%s\n", size, buf);
+
+	char file[128];
+	strcpy(file, directory);
+	int ret = sscanf(buf, "GET %s", file + strlen(file));
+	assert(ret == 1);
+
+	printf("file: %s\n\n", file);
+
+	FILE *fd = fopen(file, "r");
+	if (fd == NULL) {		
+		
+		snprintf(buf, sizeof(buf), "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\nConnection: close\r\n\r\n");
+	} else {
+		char fb[1024];
+		size = fread(fb, 1, sizeof(fb), fd);
+		assert(size >= 0);
+		fb[size] = '\0';
+
+		snprintf(buf, sizeof(buf), "HTTP/1.0 200 OK\r\nContent-Length: %d\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n%s", strlen(fb), fb);
+	}
+
+	printf("send: %d\n%s\n", strlen(buf), buf);
+
+	size = send(nd, buf, strlen(buf), MSG_NOSIGNAL);
+	assert(size == strlen(buf));
+}
+
+int main(int argc, char **argv)
+{
+	int opt;
+	while ((opt = getopt(argc, argv, "h:p:d:")) != -1) {
+		switch(opt) {
+			case 'h':
+				host = optarg;
+				break;
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case 'd':
+				directory = optarg;
+				break;
+		}
+	}
+
+	daemon(1, 0);
+
+	printf("host: %s port: %d directory: %s\n", host, port, directory);
+
+	int sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	assert(sd != -1);
+
+	int optval = 1;
+	assert(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (void *) &optval, (socklen_t) sizeof(optval)) != -1);
+
+	struct sockaddr_in sa;
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(port);
+	inet_aton(host, &sa.sin_addr);
+	
+	assert(bind(sd, (struct sockaddr *)&sa, sizeof(sa)) != -1);	
+	assert(listen(sd, SOMAXCONN) != -1);	
+
+	while(1) {		
+		int nd = accept(sd, NULL, 0);
+		assert(sd != -1);
+
+		int ret = fork();
+		if (ret == 0) {
+			http_client(nd);
+			close(nd);			
+			return 0;
+		}
+	}
+
+	close(sd);
+
+	return 0;
+}
